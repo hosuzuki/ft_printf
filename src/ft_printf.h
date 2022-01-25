bd77f4a9a36905729a9abea95d1b0b34e9886a49
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-01-25 17:11:49 +0900

    modification

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -4,23 +4,23 @@
 # include <stdio.h>
 # include <stdarg.h>
 # include <unistd.h>
+# include <stdlib.h>
 
 # define ERROR -1
-
-#	define NON 0
-# define LEFT_ALIGN 1
-# define ZERO_PAD 2
-# define NOTATION 3
-# define SIGN 4
-# define SPACE 5
+#	define OFF 0
+# define ON  1
 
 typedef struct
 {
-	va_list	args:;
+	va_list	args;
 	size_t total_len;
-	int	status;
 	char	type;
 	int len;
+	int left_align;
+	int zero_pad;
+	int notation;
+	int sign;
+	int space;
 	int	flag;
 	int width;
 	int	precision;
@@ -36,7 +36,12 @@ typedef struct
 } t_length;
 
 int	ft_printf(const char *fmt, ...);
-void	ft_analyze_fmt(const char *fmt, t_stock lst, va_list args);
+void	ft_analyze_fmt(const char *fmt, t_stock *lst);
+void ft_print_str(t_stock *lst);
+void ft_print_char(t_stock *lst);
+void	ft_print_address(t_stock *lst, uintptr_t address);
 
+void ft_print_decimal(t_stock *lst, size_t decimal);
+void ft_left_align(const char *fmt, t_stock *lst, size_t *i);
 
 #endif
