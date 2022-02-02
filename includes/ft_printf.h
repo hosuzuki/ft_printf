93884e9b9579b6717e4c62e4100c8f6722c2e1dc
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-02-02 21:30:35 +0900

    2nd draft

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hokutosuzuki <hosuzuki@student.42toky      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/30 17:23:25 by hokutosuz         #+#    #+#             */
-/*   Updated: 2022/02/01 07:09:47 by hokutosuz        ###   ########.fr       */
+/*   Updated: 2022/02/02 21:16:45 by hokutosuz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,32 +29,19 @@ typedef struct
 {
 	va_list	args;
 	size_t total_len;
-	//char	type;
 	int status;
 	int left_align;
 	int zero_pad;
-//	int notation;
 	int hash;
 	int sign;
 	int space;
-//	int	flag;
 	int width;
 	int	precision;
-
-	size_t len;
 } t_stock;
-
-typedef struct
-{
-	size_t	padding;
-	size_t	prefix;
-	size_t	body;
-	size_t	precision;
-	size_t	sum;
-} t_length;
 
 int	ft_printf(const char *fmt, ...);
 size_t	ft_analyze_fmt(const char *fmt, t_stock *lst);
+
 void ft_print_str(t_stock *lst);
 void ft_print_char(t_stock *lst);
 void	ft_print_address(t_stock *lst, size_t address);
@@ -63,11 +50,12 @@ void ft_print_decimal(t_stock *lst, int decimal);
 void	ft_print_hex(t_stock *lst, unsigned int decimal);
 void	ft_print_hex_cap(t_stock *lst, unsigned int decimal);
 
-void ft_left_align(const char *fmt, t_stock *lst, size_t *i);
-void ft_zero_pad(const char *fmt, t_stock *lst, size_t *i);
-void ft_sign(const char *fmt, t_stock *lst, size_t *i);
-void ft_space(const char *fmt, t_stock *lst, size_t *i);
-void ft_hash(const char *fmt, t_stock *lst, size_t *i);
+void ft_left_align(t_stock *lst, size_t *i);
+void ft_zero_pad(t_stock *lst, size_t *i);
+void ft_sign(t_stock *lst, size_t *i);
+void ft_space(t_stock *lst, size_t *i);
+void ft_hash(t_stock *lst, size_t *i);
+
 void ft_precision(const char *fmt, t_stock *lst, size_t *i);
 void ft_width(const char *fmt, t_stock *lst, size_t *i);
 
