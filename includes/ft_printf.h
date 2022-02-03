d6a97d3b8add98462106721c8ce4149c9de4f8c9
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-02-03 17:55:48 +0900

    tidy up for 2nd draft

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hokutosuzuki <hosuzuki@student.42toky      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/30 17:23:25 by hokutosuz         #+#    #+#             */
-/*   Updated: 2022/02/02 21:16:45 by hokutosuz        ###   ########.fr       */
+/*   Updated: 2022/02/03 17:20:36 by hokutosuz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <limits.h>
 
 # define ERROR -1
+# define GOOD 0
 #	define OFF -3
 # define ON  1
 # define PLUS 2
@@ -28,7 +29,7 @@
 typedef struct
 {
 	va_list	args;
-	size_t total_len;
+	int total_len;
 	int status;
 	int left_align;
 	int zero_pad;
@@ -40,10 +41,10 @@ typedef struct
 } t_stock;
 
 int	ft_printf(const char *fmt, ...);
-size_t	ft_analyze_fmt(const char *fmt, t_stock *lst);
+int	ft_analyze_fmt(const char *fmt, t_stock *lst);
 
-void ft_print_str(t_stock *lst);
-void ft_print_char(t_stock *lst);
+void ft_print_str(t_stock *lst, char *str);
+void ft_print_char(t_stock *lst, int c);
 void	ft_print_address(t_stock *lst, size_t address);
 void ft_print_unsigned(t_stock *lst, unsigned int decimal);
 void ft_print_decimal(t_stock *lst, int decimal);
@@ -61,8 +62,7 @@ void ft_width(const char *fmt, t_stock *lst, size_t *i);
 
 void ft_print_space(t_stock *lst, int len);
 void ft_print_sign(t_stock *lst);
-void ft_print_left_align(t_stock *lst, int len);
+void ft_print_left_align_space(t_stock *lst, int len);
 void ft_print_zero_pad(t_stock *lst, int len);
-char	*ft_itoa_l(long long n);
 
 #endif
