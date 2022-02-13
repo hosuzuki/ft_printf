970b8ba2b5429bf344bd40cf0c41810bb1bc5b3f
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-02-14 07:26:46 +0900

    first commit

--- mains/test.sh
+++ mains/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm diff.txt
+rm diff.txt pf.txt ft.txt pf.out ft.out
+make -C ../ re
 gcc -Wall -Werror -Wextra -D PRINT="printf" ../libftprintf.a main_42tester.c -o pf.out
 gcc -Wall -Werror -Wextra -D PRINT="ft_printf" ../libftprintf.a main_42tester.c -o ft.out -g
 ./pf.out >> pf.txt
