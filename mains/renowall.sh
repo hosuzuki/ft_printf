ed1a52e074558b55a62e75904c1ea781e965c016
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-02-14 11:13:15 +0900

    added no -Werror compilation version

---
+++ mains/renowall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm diff.txt pf.txt ft.txt pf.out ft.out
+make -C ../ re
+gcc -D PRINT="ft_printf" ../libftprintf.a main_42tester.c -o ft.out -g
+gcc -D PRINT="printf" ../libftprintf.a main_42tester.c -o pf.out
+./pf.out >> pf.txt
+./ft.out >> ft.txt
+diff pf.txt ft.txt >> diff.txt
--- mains/test.sh
+++ mains/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm diff.txt pf.txt ft.txt pf.out ft.out
-gcc -Wall -Werror -Wextra -D PRINT="printf" ../libftprintf.a main_42tester.c -o pf.out
 gcc -Wall -Werror -Wextra -D PRINT="ft_printf" ../libftprintf.a main_42tester.c -o ft.out -g
+gcc -Wall -Werror -Wextra -D PRINT="printf" ../libftprintf.a main_42tester.c -o pf.out
 ./pf.out >> pf.txt
 ./ft.out >> ft.txt
 diff pf.txt ft.txt >> diff.txt
