49594daba91edf6f63699e1683dd9ba10e1bc189
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-02-14 11:22:07 +0900

    change the position of ft and pf

--- mains/renowall.sh
+++ mains/renowall.sh
@@ -2,8 +2,8 @@
 
 rm diff.txt pf.txt ft.txt pf.out ft.out
 make -C ../ re
-gcc -D PRINT="ft_printf" ../libftprintf.a main_42tester.c -o ft.out -g
 gcc -D PRINT="printf" ../libftprintf.a main_42tester.c -o pf.out
+gcc -D PRINT="ft_printf" ../libftprintf.a main_42tester.c -o ft.out -g
 ./pf.out >> pf.txt
 ./ft.out >> ft.txt
 diff pf.txt ft.txt >> diff.txt
