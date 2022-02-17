4268c2c822bd7ccab4acd93b9b859dfca66e52b2
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-02-17 11:19:18 +0900

    added test

--- mains/main_ft_printf_tester.c
+++ mains/main_ft_printf_tester.c
@@ -7,11 +7,13 @@ void end(void)__attribute__((destructor));
 
 void end(void)
 {
-	    system("leaks a.out");
+	    system("leaks ft.out");
 }
 
 int main(void)
 {
+	PRINT(" --- Return : %d\n", PRINT("%*d", INT_MAX - 1, 8)); //T23
+/*
 	PRINT(" --- Return : %d\n", PRINT("[%0*d]\n", -4, 8)); //T23
 	PRINT(" --- Return : %d\n", PRINT("[%-3.2d]\n", 8)); //T23
 	PRINT(" --- Return : %d\n", PRINT("[%.*d]\n", -4, 8)); //T23
@@ -20,5 +22,6 @@ int main(void)
 	PRINT(" --- Return : %d\n", PRINT("[%10c]\n", 'b')); //T23
 	PRINT("%\n");
 	PRINT("%%%%%%%\n");
+*/
 	return (0);
 }
