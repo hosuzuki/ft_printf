67e58fa4b615550b2beaebf717911cb1a52e462c
hosuzuki <hokuto.suzuki@gmail.com>
2023-10-17 13:15:18 +0900

    :recycle: delete shell script / modify Makefile little

--- test/nowall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm diff.txt pf.txt ft.txt pf.out ft.out
-gcc -D PRINT="printf" ../libftprintf.a main_42tester.c -o pf.out
-gcc -D PRINT="ft_printf" ../libftprintf.a main_42tester.c -o ft.out -g
-./pf.out >> pf.txt
-./ft.out >> ft.txt
-diff ft.txt pf.txt >> diff.txt
